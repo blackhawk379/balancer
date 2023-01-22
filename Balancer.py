67ea7256bd1025fa557795a9981e2ca50be5e5d8
blackhawk379 <78206703+blackhawk379@users.noreply.github.com>
2023-01-22 20:57:47 +0530

    Update Balancer.py

--- Balancer.py
+++ Balancer.py
@@ -302,8 +302,10 @@ with salary:
         empid = get_info('Empid', session['user'])
         salary = get_sheet('SalarySheet')
         paymentdf = get_sheet('Payments')
+        balance = get_sheet('BalanceSheet')
         if role == 'Simple':
             st.header('Your Salary Distribution')
+            st.write('Current Balance' + str(list(balance.loc[balance['Empid'] == empid]['Current Balance'])[0]))
             col1, col2 = st.columns(2)
             index = salary.index[salary['Empid'] == empid].tolist()[0]
             col1.dataframe(pd.DataFrame(list(zip(list(salary.columns), list(salary.iloc[index])))))
@@ -311,11 +313,13 @@ with salary:
         else:
             personal, allBalance = st.tabs(['Personal', 'All'])
             with personal:
-                st.header('Your Salary Distribution')
+                st.header('Your Salary Statement')
+                st.write('Current Balance' + str(list(balance.loc[balance['Empid'] == empid]['Current Balance'])[0]))
                 col11, col21 = st.columns(2)
                 index = salary.index[salary['Empid'] == empid].tolist()[0]
                 col11.dataframe(pd.DataFrame(list(zip(list(salary.columns), list(salary.iloc[index])))))
                 col21.dataframe(paymentdf.loc[paymentdf['Empid'] == empid])
+                
             with allBalance:
                 st.header("Salary Sheet")
                 salarySheet = get_sheet('SalarySheet')
