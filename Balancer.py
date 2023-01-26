293d7bdbbd202edfc6775e6f387324d4597428b5
blackhawk379 <78206703+blackhawk379@users.noreply.github.com>
2023-01-26 15:09:14 +0530

    Update Balancer.py

--- Balancer.py
+++ Balancer.py
@@ -99,13 +99,16 @@ def display_salary():
                 tbody th {display:none}
                 </style>
                 """
+
     # Inject CSS with Markdown
     _col1.markdown(hide_table_row_index, unsafe_allow_html=True)
+    st.write(salaryDf.index[salaryDf['EmpId'] == empId)
     _index = salaryDf.index[salaryDf['EmpId'] == empId].tolist()[0]
     _title, _data = list(salaryDf.columns), list(salaryDf.iloc[_index])
     _df = pd.DataFrame(list(zip(_title[1:], _data[1:])))
     _df.columns = [_title[0], _data[0]]
     _col1.table(_df)
+
     _col2.dataframe(paymentDf.loc[paymentDf['EmpId'] == empId])
 
 def display_all(title, sheet, key):
